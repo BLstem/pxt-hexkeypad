56f6b9fcbbdef394db307c63096b83253d5a5462
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-07-26 16:15:14 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,18 +5,20 @@ namespace keypad {
     let padnumber = [["1", "2", "3", "A"], ["4", "5", "6", "B"], ["7", "8", "9", "C"], ["*", "0", "#", "D"]]
     let matrix: number[]
 
-    /** This block has to be under "forever" loop. 
-     *  
+    /** Initialize pins connected to the Hex Keypad. 
+     *  Default pins connection is set as follow
+     *  P1, P2, P6, P9, P10, P12, P15, P16
+     *  You can change pins connection as you want. 
     */
     //%block="Initialize row 1 %row1|row 2 %row2|row 3 %row3|row 4 %row4|col 1 %col1|col 2 %col2|col 3 %col3|col 4 %col4"
     //%blockId=initialize_key_pad
     //%blockExternalInputs=true
     //%row1.fieldEditor="gridpicker" row1.fieldOptions.columns=5 row1.defl=DigitalPin.P1
     //%row2.fieldEditor="gridpicker" row2.fieldOptions.columns=5 row2.defl=DigitalPin.P2
-    //%row3.fieldEditor="gridpicker" row3.fieldOptions.columns=5 row3.defl=DigitalPin.P6
-    //%row4.fieldEditor="gridpicker" row4.fieldOptions.columns=5 row4.defl=DigitalPin.P9
-    //%col1.fieldEditor="gridpicker" col1.fieldOptions.columns=5 col1.defl=DigitalPin.P10
-    //%col2.fieldEditor="gridpicker" col2.fieldOptions.columns=5 col2.defl=DigitalPin.P12
+    //%row3.fieldEditor="gridpicker" row3.fieldOptions.columns=5 row3.defl=DigitalPin.P8
+    //%row4.fieldEditor="gridpicker" row4.fieldOptions.columns=5 row4.defl=DigitalPin.P12
+    //%col1.fieldEditor="gridpicker" col1.fieldOptions.columns=5 col1.defl=DigitalPin.P13
+    //%col2.fieldEditor="gridpicker" col2.fieldOptions.columns=5 col2.defl=DigitalPin.P14
     //%col3.fieldEditor="gridpicker" col3.fieldOptions.columns=5 col3.defl=DigitalPin.P15
     //%col4.fieldEditor="gridpicker" col4.fieldOptions.columns=5 col4.defl=DigitalPin.P16
     export function initialize(row1: DigitalPin, row2: DigitalPin, row3: DigitalPin, row4: DigitalPin, col1: DigitalPin, col2: DigitalPin, col3: DigitalPin, col4: DigitalPin): void {
@@ -24,7 +26,7 @@ namespace keypad {
     }
 
     /** Read what you have just pressed and return a string.  
-     *  If the key pad is not pressed, it will return a empty string, i.e. "". 
+     *  If keypad is not pressed, this function will return a empty string, i.e. "". 
     */
     //%block="reading"
     //%blockId="reading_from_the_pad"
